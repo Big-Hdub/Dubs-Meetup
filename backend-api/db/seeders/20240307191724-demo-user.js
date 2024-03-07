1135a4a99d11a4c5d32b853da3ce62ea89147cdc
Big-Hdub <hdub-bb@hotmail.com>
2024-03-07 12:38:33 -0800

    created Model, migration, and seeder for testing.

---
+++ backend-api/db/seeders/20240307191724-demo-user.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+const demoUsers = [
+  {
+    firstName: 'demo',
+    lastName: 'user',
+    email: 'demo@user.io',
+    username: 'Demo-lition',
+    hashedPassword: bcrypt.hashSync('password')
+  },
+  {
+    firstName: 'Harry',
+    lastName: 'Wagner',
+    email: 'user1@user.io',
+    username: 'FakeUser1',
+    hashedPassword: bcrypt.hashSync('password2')
+  },
+  {
+    firstName: 'Kumiko',
+    lastName: 'Wagner',
+    email: 'user2@user.io',
+    username: 'FakeUser2',
+    hashedPassword: bcrypt.hashSync('password3')
+  }
+]
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    try {
+      await User.bulkCreate(demoUsers, { validate: true });
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
