f8290e5559b9b760b8cdc29fc0158f73cde6a2c3
Big-Hdub <hdub-bb@hotmail.com>
2024-03-07 09:53:43 -0800

    Added api router and changed folder naming for backend from api to backend-api

--- backend-api/routes/index.js
+++ backend-api/routes/index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
+
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
@@ -10,5 +13,7 @@ router.get("/api/csrf/restore", (req, res) => {
         'XSRF-Token': csrfToken
     });
 });
+
+
 
 module.exports = router;
