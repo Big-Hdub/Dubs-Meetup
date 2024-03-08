9faa4c1d2151a932b40a869145455f7f17767522
Big-Hdub <hdub-bb@hotmail.com>
2024-03-08 14:45:43 -0800

    added associations, validations, and constraints of Members model and migration

--- backend-api/db/migrations/20240308210015-create-member.js
+++ backend-api/db/migrations/20240308210015-create-member.js
@@ -10,21 +10,26 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       userId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
       groupId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
       status: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     });
   },
--- backend-api/db/models/member.js
+++ backend-api/db/models/member.js
@@ -10,13 +10,43 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
     }
   }
   Member.init({
-    userId: DataTypes.INTEGER,
-    groupId: DataTypes.INTEGER,
-    status: DataTypes.STRING
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: {
+          msg: "Must be an integer of User id"
+        },
+        isInt: {
+          msg: "Must be an integer of User id"
+        }
+      }
+    },
+    groupId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: {
+          msg: "Must be an integer of Group id"
+        },
+        isInt: {
+          msg: "Must be an integer of Group id"
+        }
+      }
+    },
+    status: {
+      type: DataTypes.STRING,
+      defaultValue: 'pending',
+      validate: {
+        isIn: {
+          args: [['co-host', 'member', 'pending']],
+          msg: 'Must be "co-host", "member, or "pending"'
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'Member',
