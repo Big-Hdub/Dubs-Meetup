8950edc5c766215f3d3843135238cdb906173bd1
Big-Hdub <hdub-bb@hotmail.com>
2024-03-11 23:14:40 -0700

    added options to migrations and seeders

--- backend-api/db/migrations/20240308210015-create-member.js
+++ backend-api/db/migrations/20240308210015-create-member.js
@@ -1,4 +1,10 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -31,7 +37,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Members');
