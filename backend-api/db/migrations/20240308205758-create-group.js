1136b164cbbb6bb071b51d3d2eb98f3dc1296adf
Big-Hdub <hdub-bb@hotmail.com>
2024-03-29 21:00:43 -1000

    added removeIndex for groups

--- backend-api/db/migrations/20240308205758-create-group.js
+++ backend-api/db/migrations/20240308205758-create-group.js
@@ -64,5 +64,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable(options);
+    await queryInterface.removeIndex(options, ['organizerId', 'name'], { unique: true })
   }
 };
