73ced091a2541dee3260691d2ccf6d368ef3a203
Big-Hdub <hdub-bb@hotmail.com>
2024-05-02 20:29:03 -0700

    merging create front end build to main

--- backend-api/routes/index.js
+++ backend-api/routes/index.js
@@ -16,13 +16,13 @@ if (process.env.NODE_ENV === 'production') {
     });
 
     // Serve the static assets in the frontend's build folder
-    router.use(express.static(path.resolve("../frontend/build")));
+    router.use(express.static(path.resolve("../frontend/dist")));
 
     // Serve the frontend's index.html file at all other routes NOT starting with /api
     router.get(/^(?!\/?api).*/, (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
         return res.sendFile(
-            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+            path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
         );
     });
 }
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
+        return res.json({ 'XSRF-Token': csrfToken });
     });
 }
 
