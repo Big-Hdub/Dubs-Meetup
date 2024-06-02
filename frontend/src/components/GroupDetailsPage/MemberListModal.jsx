f74a7a49b2304f91f41560e69693064fad3e63b9
Big-Hdub <hdub-bb@hotmail.com>
2024-06-02 16:35:00 -0700

    cleaned up MemberListModal.jsx

--- frontend/src/components/GroupDetailsPage/MemberListModal.jsx
+++ frontend/src/components/GroupDetailsPage/MemberListModal.jsx
@@ -1,10 +1,7 @@
-// import OpenModalButton from "../OpenModalButton/OpenModalButton";
 import { useSelector } from "react-redux";
 import * as memberActions from "../../store/members";
 import './MemberList.css'
 import MemberForm from "./MemberForm";
-
-
 
 const MemberListModal = ({ session, groupId }) => {
     const members = useSelector(memberActions.selectMember).entities;
